e79f8902db3d303388ed75df2b740b63141f82fc
Safwankaprakkadan <safwankc1999@gmail.com>
2023-04-13 11:18:15 +0530

    n

--- src/app/hostel/hostel-routing.module.ts
+++ src/app/hostel/hostel-routing.module.ts
@@ -7,13 +7,10 @@ import { LeaveApplicationComponent } from './leave-application/leave-application
 import { AddRoomComponent } from './add-room/add-room.component';
 import { AssetsComponent } from './assets/assets.component';
 import { OutpassComponent } from './outpass/outpass.component';
-<<<<<<< HEAD
 import { HostelFeeComponent } from './hostel-fee/hostel-fee.component';
 import { FeeReductionComponent } from './fee-reduction/fee-reduction.component';
-=======
 import { MessFeeComponent } from './mess-fee/mess-fee.component';
 import { TotallBillGenerateComponent } from './totall-bill-generate/totall-bill-generate.component';
->>>>>>> bfcba7a3e48778c12c8ed140e881efa15115bf56
 
 
 const routes: Routes = [
@@ -25,13 +22,10 @@ const routes: Routes = [
   { path:'add_room',component:AddRoomComponent},
   { path:'assets',component:AssetsComponent},
   { path:'outpass',component:OutpassComponent},
-<<<<<<< HEAD
   { path:'hostel_fee',component:HostelFeeComponent},
-  { path:'fee_reduction',component:FeeReductionComponent}
-=======
+  { path:'fee_reduction',component:FeeReductionComponent},
   { path:'messfee',component:MessFeeComponent},
   { path:'total_bill',component:TotallBillGenerateComponent},
->>>>>>> bfcba7a3e48778c12c8ed140e881efa15115bf56
 
 
 ];
